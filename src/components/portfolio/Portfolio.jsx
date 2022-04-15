d964723a5778e39b65851acb6d19a015b162036c
Djurica Surla <djurica.surla@gmail.com>
2022-04-15 15:51:59 +0200

    Added another website to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import './portfolio.css';
 import IMG1 from '../../assets/Portfolio1.PNG';
 import IMG2 from '../../assets/Portfolio2.PNG';
 import IMG3 from '../../assets/Portfolio3.PNG';
+import IMG5 from '../../assets/Portfolio5.PNG';
 import IMG4 from '../../assets/Portfolio4.PNG';
 
 const data = [
@@ -14,19 +15,26 @@ const data = [
     demo: 'https://camping-serbia.herokuapp.com/',
   },
   {
-    id: 1,
+    id: 2,
     image: IMG2,
     title: 'StudyBuddy - Django',
     github: 'https://github.com/DjuricaSurla/Django-StudyBudd-Project',
     demo: 'https://study-buddy-project.herokuapp.com/',
   },
   {
-    id: 1,
+    id: 3,
     image: IMG3,
     title: 'GoalSetter - MERN Stack',
     github:
       'https://github.com/DjuricaSurla/MERN-GoalSetterApp-Project/tree/master',
     demo: 'https://goal-setter-app-mern.herokuapp.com/',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'MyNotes - Django, React',
+    github: 'https://github.com/DjuricaSurla/DjangoReact-MyNotes-Project',
+    demo: 'https://my-notes-app1.herokuapp.com/',
   },
   {
     id: 4,
